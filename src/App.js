c4d633727b259b71565d87a6149b4182af6b5cad
jeremytjimenez <jeremytjimenez@gmail.com>
2023-05-27 00:19:42 -0400

    created video page component with basic static player + install react-youtube and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import VideoPage from './Components/VideoPage'
+import { Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-
+      <VideoPage />
     </div>
   );
 }
