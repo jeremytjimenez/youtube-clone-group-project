56618db681d4256ccea0df269f7d8965b9be70fb
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-05-28 00:04:38 -0400

    Dev bio propogating data dynamically, need to create bio blurbs add images, nav bar and borders and to make gitHub link clickable to our pages. Added to App level just to display may need to be removed to avoid conflict

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import Devbio from './Devbio';
+fetch(
+  `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
+);
 function App() {
   return (
     <div className="App">
-
+<Devbio/>
     </div>
   );
 }
---
+++ src/Devbio.js
@@ -0,0 +1,51 @@
+import React from 'react'
+const devs= [
+    {
+        firstName: "Jeremy",
+        lastName: "Jimenez",
+        age: 24, 
+        bio: "Jeremy is a dev with ......",
+        gitHub: "https://github.com/jeremytjimenez"
+    },
+    {
+        firstName: "Hillary",
+        lastName: "Brito",
+        age: 25, 
+        bio: "Hillary is a dev with ......",
+        gitHub: "https://github.com/HillaryB110"
+    },
+    
+]
+const projectDescription = [
+    {
+        title : "JHILLMedia Youtube Clone Project",
+        projectInfo: "A group project to build a youtube clone, utilizing React to create an attractive and functional website. Inspired by the vaporwave aesthetic to create a retro feel."
+    }
+]
+function Devbio() {
+  return (
+    <div className="card my-4 border-3 border-primary-subtle text-center">
+      {devs.map((dev, index) => (
+        <div key={index}>
+          <h2>
+            {dev.firstName} {dev.lastName}
+          </h2>
+          <p>Age: {dev.age}</p>
+          <p>{dev.bio}</p>
+          <p>{dev.gitHub}</p>
+        </div>
+      ))}
+      <h3>Project Description:</h3>
+      {projectDescription.map((project, index) => (
+        <div key= {index}>
+          <h4>{project.title}</h4>
+          <p>{project.projectInfo}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+
+export default Devbio
