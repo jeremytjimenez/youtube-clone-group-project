c0325341050e436dd60593bcf41b47fb1c3636b5
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-05-30 14:49:48 -0400

    Created the Navbar used bootstrap to impliment some basic styling, stll need the drop down menu to propagate correctly as well as created the link to alter the URL so that the routes can be added to the appropriate components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import VideoPage from "./Components/VideoPage";
+// import { Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css"; 
 
 fetch(
   `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
 );
-import VideoPage from './Components/VideoPage'
-import { Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <VideoPage />
     </div>
   );
