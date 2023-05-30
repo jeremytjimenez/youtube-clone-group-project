4187a4b8c2cc9157f4640f61e6c3a50161b3f266
jeremytjimenez <jeremytjimenez@gmail.com>
2023-05-30 19:48:06 -0400

    added navigation functionality

--- src/Components/VideoPage.js
+++ src/Components/VideoPage.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import YouTube from 'react-youtube'
+import { useParams } from 'react-router-dom'
 
 function VideoPage() {
+  const params = useParams()
+
+//   const opts: YouTubeProps['opts'] = {
+//     height: '390',
+//     width: '640',
+//     playerVars: {
+//       // https://developers.google.com/youtube/player_parameters
+//       autoplay: 1,
+//     },
+//   };
+
   return (
     <div>
-        <YouTube videoId="0t6U9iRgD7k" />
+        <YouTube videoId={params.id} />
     </div>
   )
 }
